b594ba6584abc1e4171cffad6da00bc3657f1848
SnowballSH <derek070211@gmail.com>
2020-11-12 23:04:22 -0600

    fix

--- src/runtime/builtin_types/object.rb
+++ src/runtime/builtin_types/object.rb
@@ -5,12 +5,16 @@ Ref = {
   "": 'dead'
 }.freeze
 
+def refer(x)
+  Ref.key?(x.value.to_s.to_sym) ? Ref[x.value.to_s.to_sym] : x.value
+end
+
 $constants['Object'].def :pln, 1 do |_receiver, arguments|
-  puts(*arguments.map { |x| Ref.key?(x.value.to_s.to_sym) ? Ref[x.value.to_s.to_sym] : x.value }.join(' '))
+  puts(*arguments.map { |x| refer(x) }.join(' '))
   $constants['dead']
 end
 
 $constants['Object'].def :print, 1 do |_receiver, arguments|
-  print(*arguments.map { |x| Ref.key?(x.value.to_sym) ? Ref[x.to_sym] : x.value }.join(' '))
+  print(*arguments.map { |x| refer(x) }.join(' '))
   $constants['dead']
 end
